f33c73fac8f1ca788c50069c6239b4007a981ccf
djbb7 <daniel.bruzual@gmail.com>
2016-02-06 21:33:50 +0100

    Add exception for faulty dependent server

---
+++ src/fitbot/bls/exception/ServerCommunicationException.java
@@ -0,0 +1,14 @@
+package fitbot.bls.exception;
+
+public class ServerCommunicationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7373733210366402495L;
+	
+	public ServerCommunicationException(String message){
+		super(message);
+	}
+	
+}
